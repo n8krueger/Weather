d41853d782bf1fbf7b94a0b6425043c13621d505
NathanKrueger <n8krueger@gmail.com>
2026-03-09 13:16:43 -0400

    updates

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,24 @@
-import javax.sound.midi.Soundbank;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
     private static Scanner scanner = new Scanner(System.in);
+    private static String zipcode = "";
+    private static Weather weather;
 
     public static void main(String[] args) throws IOException {
-        //Weather weather = new Weather();
+        weather = new Weather();
+
 
         boolean running = true;
 
         while (running) {
             System.out.print("Enter your zipcode: ");
-            String zipcode = getInput();
+            zipcode = getInput();
 
             if (validateZipcode(zipcode)) {
-                showMenu();
+                running = menu();
             }
             else {
                 System.out.println("Invalid zipcode, please try again.");
@@ -26,40 +28,85 @@ public class Main {
         scanner.close();
     }
 
-    public static void subMenu() {
+    public static boolean menu() {
         boolean back = false;
 
         while (!back) {
             showMenu();
-            int choice = getInput();
 
+            String choice = getInput();
+
+            if(validateMenuInput(choice)) {
+                switch (choice) {
+                    case "1":
+                        weather.getCurrentWeather(zipcode);
+                        break;
+                    case "2":
+                        System.out.println("Here is the 5-day forecast for " + zipcode);
+                        break;
+                    case "3":
+                        back = true;
+                        break;
+                    case "0":
+                        return false;
+                }
+            }
+            else {
+                System.out.println("Invalid menu choice, please try again.");
+            }
 
         }
+
+        return true;
     }
 
     private static void showMenu() {
         System.out.println("---- Menu ----");
         System.out.println("1. Get current weather");
         System.out.println("2. Get 5-day forecast");
-        System.out.println("0. Enter new zipcode");
+        System.out.println("3. Enter new zipcode");
+        System.out.println("0. Quit");
         System.out.print("Enter your choice: ");
     }
 
     private static String getInput() {
         while(!scanner.hasNext()) {
-            System.out.print("Please enter a number: ");
             scanner.next();
         }
 
         return scanner.next();
     }
 
-    public static boolean validateZipcode(String zipcode) {
-
-
-        if (Integer.parseInt(zipcode) < 501 || Integer.parseInt(zipcode) > 99734) {
-            return true;
+    public static boolean validateZipcode(String sZipcode) {
+        try {
+            int zipcode = Integer.parseInt(sZipcode);
+            if (zipcode<501||zipcode>99734) {
+                // zipcode was outside range
+                return false;
+            }
         }
-        return false;
+        catch (NumberFormatException e) {
+            // zipcode was not numeric
+            return false;
+        }
+
+        // else zipcode valid
+        return true;
     }
+
+    public static boolean validateMenuInput(String sChoice) {
+        try {
+            int choice = Integer.parseInt(sChoice);
+            if (choice < 0 || choice > 3) {
+                // choice was outside of range
+                return false;
+            }
+        }
+        catch (NumberFormatException e) {
+            // choice was not numeric
+            return  false;
+        }
+        return true;
+    }
+
 }
--- src/main/java/Weather.java
+++ src/main/java/Weather.java
@@ -3,10 +3,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -18,19 +15,19 @@ public class Weather {
     Map<String, String> config;
 
     public Weather() throws IOException {
-        config = loadConfig();
+        loadConfig();
     }
 
     public Map<String, String> loadConfig() throws IOException {
         InputStream inputStream = Files.newInputStream(Paths.get("config.yml"));
         Yaml yaml = new Yaml();
-        Map<String, String> config = yaml.load(inputStream);
+        config = yaml.load(inputStream);
         return config;
     }
 
-    public void getCurrentWeather(String zipCode) {
+    public void getCurrentWeather(String zipcode) {
         String url = config.get("baseURL") + config.get("currentEndPt");
-        String paramString = "?key=" + config.get("apiKey") + "&q=" + zipCode;
+        String paramString = "?key=" + config.get("apiKey") + "&q=" + zipcode;
 
         HttpClient client = HttpClient.newHttpClient();
 
@@ -46,12 +43,13 @@ public class Weather {
             String jsonBody = response.body();
             ObjectMapper objectMapper = new ObjectMapper();
 
-            Map<String, Object> map = objectMapper.readValue(jsonBody, new TypeReference<Map<String, Object>>() {});
+            Map<String, Object> map = objectMapper.readValue(jsonBody, new TypeReference<>() {});
             Map<String, String> locationMap = (Map<String, String>) map.get("location");
+            Map<String, Object> currentMap = (Map<String, Object>) map.get("current");
+            Map<String, String> conditionsMap = (Map<String, String>) currentMap.get("condition");
 
-            System.out.println("Status Code: " + response.statusCode());
-            System.out.println("Location: " + locationMap.get("name") + ", " + locationMap.get("region"));
-
+            //System.out.println("Status Code: " + response.statusCode());
+            System.out.println("The current weather for " + locationMap.get("name") + ", " + locationMap.get("region") + " is " + currentMap.get("temp_f") + "F, " + conditionsMap.get("text"));
         }
         catch (IOException | InterruptedException e)
         {
